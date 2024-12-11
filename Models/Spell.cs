6930ff5b5caefa5f4bba4e2e2e84ff4ee2aa023e
Lucas Pomar <lucaspomar@outlook.com>
2024-12-11 16:59:43 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Spell.cs
@@ -0,0 +1,22 @@
+﻿namespace SkyfallAPI.Models;
+
+public class Spell
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public string? Action { get; set; }
+    public int Cost { get; set; }
+    public string? Description { get; set; }
+    public string? Execution { get; set; }
+    public string? Range { get; set; }
+    public string? Target { get; set; }
+    public string? Components { get; set; }
+    public string? Duration { get; set; }
+    public string? Attack { get; set; }
+    public string? Effect { get; set; }
+    public string? Miss { get; set; }
+    public string? Special { get; set; }
+
+    //public string Layer { get; set; }
+    //public string Tags { get; set; }
+}
