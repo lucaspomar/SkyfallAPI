34d486b0905fc352266bae7ee0e14f6dfe85e4e6
Lucas Pomar <lucaspomar@outlook.com>
2025-01-31 16:39:27 -0300

    Create empty SpellController

---
+++ Controllers/SpellController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SkyfallAPI.Models;
+
+namespace SkyfallAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpellController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Spell>> GetAllSpells()
+        {
+            return Ok();
+        }
+    }
+}
