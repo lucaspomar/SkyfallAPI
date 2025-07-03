ee40d1e68a82000218a1c5749ce58cd9784486c4
Lucas Pomar <lucaspomar@outlook.com>
2025-07-03 17:50:52 -0300

    Create HomeContent Component

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TopbarComponent } from '../topbar/topbar.component';
+import { HomeContentComponent } from '../home-content/home-content.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    TopbarComponent
+    TopbarComponent,
+    HomeContentComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
