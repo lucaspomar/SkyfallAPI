4b7d8279f15d363779abcfd94dfdb9d549de2323
Lucas Pomar <lucaspomar@outlook.com>
2025-07-03 21:35:20 -0300

    Add anchors to topbar

--- Frontend/src/app/components/topbar/topbar.component.ts
+++ Frontend/src/app/components/topbar/topbar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from '@angular/material/icon'
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-topbar',
   imports: [
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterLink,
+    RouterLinkActive,
+    MatButtonModule
   ],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css'
