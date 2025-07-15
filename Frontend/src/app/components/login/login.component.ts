b9fdaffe8bf58de5338ea7423ddcc86ec44cc3b5
Lucas Pomar <lucaspomar@outlook.com>
2025-07-15 13:12:40 -0300

    Create login component

---
+++ Frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TopbarComponent } from '../topbar/topbar.component';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    TopbarComponent
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
